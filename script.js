694eed9b193f47553f5648951a9a0af91cbd1ab2
Vitam Discipuli <discipulicalculo.gmail.com>
2021-09-19 18:30:42 -0700

    Created game board

---
+++ script.js
@@ -0,0 +1,12 @@
+let gameBoard = (function() {
+    let board = [,,,,,,,,]
+
+  })();
+  
+  let displayController = (function() {
+  
+  })();
+  
+  
+  let squares = document.getElementsByClassName('gamesquare');
+  console.log(squares.length);
