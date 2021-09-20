b598d0cd4e5c1159debe9faaa9fd97dc783ea398
Vitam Discipuli <discipulicalculo.gmail.com>
2021-09-19 18:59:04 -0700

    Added content render function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let gameBoard = (function() {
-    let board = [,,,,,,,,]
+    let board = ['','X','','O','','','','X','X']
+    let squares = document.getElementsByClassName('gamesquare')
 
-  })();
+    for (i = 0; i < squares.length; i++) {
+      squares[i].innerHTML = board[i];
+    };
+
+})();
   
-  let displayController = (function() {
+let displayController = (function() {
   
-  })();
+})();
   
   
-  let squares = document.getElementsByClassName('gamesquare');
-  console.log(squares.length);
+let squares = document.getElementsByClassName('gamesquare');
+console.log(squares.length);
