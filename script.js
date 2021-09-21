b5c95f32b9a9c517e3edb2f08b0c0dfe8bd3cb7a
Vitam Discipuli <discipulicalculo.gmail.com>
2021-09-20 20:04:41 -0700

    Added marking functionality

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 let gameBoard = (function() {
-    let board = ['','X','','O','','','','X','X']
+    let board = ['','','','','','','','','']
     let squares = document.getElementsByClassName('gamesquare')
 
     for (i = 0; i < squares.length; i++) {
       squares[i].innerHTML = board[i];
     };
 
+    for (i = 0; i < squares[i].length; i++) {
+      if (squares[i].innerHTML === '') {
+        squares[i].addEventListener('click', (e) => {
+          e.currentTarget.innerHTML = 'X'
+        })
+      }
+    }
+
 })();
   
+let game = (function() {
+  player1 = createPlayer('player1');
+  player1.symbol = 'X'
+  player2 = createPlayer('player2');
+  player2.symbol = 'O'
+
+
+})
 let displayController = (function() {
   
 })();
-  
-  
-let squares = document.getElementsByClassName('gamesquare');
-console.log(squares.length);
+
+function createPlayer(name) {
+  name: name;
+  symbol: '';
+
+  return {
+    name,
+    symbol
+  }
+}
+
+console.log(player1.symbol)
